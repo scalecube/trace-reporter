5dc714dfca17aeba8bf0361e8320de20965640f3
ronen <ronen.hamias@gmail.com>
2019-03-09 18:37:18 +0200

    improvements

--- src/test/java/io/scalecube/trace/Demo.java
+++ src/test/java/io/scalecube/trace/Demo.java
@@ -4,43 +4,28 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.concurrent.ExecutionException;
 
 public class Demo {
 
   private static String folder = "./target/traces/";
   
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws Exception{
 
     new File(folder).mkdir();
-    
-    Reporter r = new Reporter();
 
-    TraceData trace = new TraceData("trace-1");
+    TraceReporter r = new TraceReporter();
     
-    trace.xaxis().add(1d);
-    trace.xaxis().add(2d);
-    trace.xaxis().add(3d);
-
-    trace.yaxis().add(31d);
-    trace.yaxis().add(13d);
-    trace.yaxis().add(12d);
-
-    OutputStream out = new FileOutputStream(folder + "out-1.json");
-    r.dump(out, trace);
-    out.close();
-
-    trace.xaxis().add(1d);
-    trace.yaxis().add(31d);
+    r.trace("reactor-netty");
+    r.addY("reactor-netty",11);
+    r.addY("reactor-netty",22);
+    r.addY("reactor-netty",33);
     
-    trace.xaxis().add(2d);
-    trace.yaxis().add(13d);
+    r.trace("reactor-aeron");
+    r.addX("reactor-aeron",10);
+    r.addX("reactor-aeron",20);
+    r.addX("reactor-aeron",30);
     
-    trace.xaxis().add(3d);
-    trace.yaxis().add(12d);
-    
-    r.dumpToFile(folder + "out-2.json",trace);
-    
-    r.dumpToFile(folder , "out-2.json", trace);
-    
+    r.dumpTo(folder).get();
   }
 }
