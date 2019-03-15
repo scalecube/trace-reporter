a8e1ce52073d3e320aa994ebec127e5f4fdb22a9
ronen <ronen.hamias@gmail.com>
2019-03-15 20:57:39 +0200

    update script

--- scripts/chart_reporter.sh
+++ scripts/chart_reporter.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# possible environment variables:
+# $TRACES_FOLDER : folder where traces files are generated
+# $CHARTS_FOLDER : folder where chart reports is created
+# $CHART_TEMPLATE : based on which chart is created
+
 cd $(dirname $0)
 cd ../
 
