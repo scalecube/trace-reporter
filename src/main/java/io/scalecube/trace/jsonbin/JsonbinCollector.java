a88b146b64ea47fdfb1b023729fcafd830dd2ff4
ronen <ronen.hamias@gmail.com>
2019-03-15 12:11:46 +0200

    remove

--- src/main/java/io/scalecube/trace/jsonbin/JsonbinCollector.java
+++ src/main/java/io/scalecube/trace/jsonbin/JsonbinCollector.java
@@ -1,18 +1,13 @@
 package io.scalecube.trace.jsonbin;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.scalecube.trace.TraceReporter;
 import java.io.File;
-import java.io.IOException;
 
 public class JsonbinCollector {
-
-  private static String secret = "$2a$10$TwUCv3gNTtJ.wCQOFr.LAuedqa3y296q8dSODzSP26jPn/I.U4olO";
-  private static String collectionId = "5c87f177bb08b22a7568ee7a";
 
   private static String in_folder = "./target/traces/";
   private static String out_folder = "./target/charts/report.json";
@@ -26,17 +21,19 @@ public class JsonbinCollector {
 
     ArrayNode traces = mapper.createArrayNode();
     for (String file : in_dir.list()) {
-      traces.add("https://api.jsonbin.io/b/"+file);
+      traces.add("https://api.jsonbin.io/b/" + file);
     }
-    
+
     ((ObjectNode) root).put("traces", traces);
 
     root.path("traces");
-    
-    r.sendToJsonbin(root).subscribe(consumer -> {
-      System.out.println(consumer); 
-      System.out.println("https://api.jsonbin.io/b/"+ consumer.id()); 
-    });
+
+    r.sendToJsonbin(root)
+        .subscribe(
+            consumer -> {
+              System.out.println(consumer);
+              System.out.println("https://api.jsonbin.io/b/" + consumer.id());
+            });
     Thread.currentThread().join();
   }
 }
