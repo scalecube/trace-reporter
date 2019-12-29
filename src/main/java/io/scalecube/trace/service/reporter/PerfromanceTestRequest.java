5824f51568956d6a18673baa3cd6abad0084c639
Ronen Nachmias <ronen.hamias@gmail.com>
2019-12-29 11:23:55 +0200

    check style

--- src/main/java/io/scalecube/trace/service/reporter/PerfromanceTestRequest.java
+++ src/main/java/io/scalecube/trace/service/reporter/PerfromanceTestRequest.java
@@ -3,7 +3,7 @@ package io.scalecube.trace.service.reporter;
 import io.scalecube.trace.TraceData;
 
 public class PerfromanceTestRequest {
-	
+
   private final String sha;
   private final TraceData[] traces;
   private String owner;
@@ -11,7 +11,7 @@ public class PerfromanceTestRequest {
 
   /**
    * a DTO request to report a perfromance tests resutls.
-   * 
+   *
    * @param owner the owner repository of this request.
    * @param repo the repository of the request.
    * @param sha the sha commit context of the request.
@@ -23,5 +23,4 @@ public class PerfromanceTestRequest {
     this.sha = sha;
     this.traces = traces;
   }
-  
 }
