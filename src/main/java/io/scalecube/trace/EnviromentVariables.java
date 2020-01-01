2956b684509b11ab385ec749fc3cb352c509b302
Ronen Nachmias <ronen.hamias@gmail.com>
2020-01-01 07:51:01 +0200

    refactoring

---
+++ src/main/java/io/scalecube/trace/EnviromentVariables.java
@@ -0,0 +1,50 @@
+package io.scalecube.trace;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class EnviromentVariables {
+
+  private static final Dotenv dotenv =
+      Dotenv.configure()
+          .directory("./src/main/resources/")
+          .ignoreIfMalformed()
+          .ignoreIfMissing()
+          .load();
+
+  public static String env(String name) {
+    return dotenv.get(name);
+  }
+
+  public static String env(String name, String defaultValue) {
+    return dotenv.get(name, defaultValue);
+  }
+
+  public static boolean isActive() {
+    return EnviromentVariables.env("TRACE_REPORT") != null
+        && EnviromentVariables.env("TRACE_REPORT").equals("true");
+  }
+
+  public static String owner(String defValue) {
+    return env("OWNER", defValue);
+  }
+
+  public static String repo(String defValue) {
+    return env("REPO", defValue);
+  }
+
+  public static String sha(String defValue) {
+    return env("COMMIT_ID", defValue);
+  }
+
+  public static String url(String defValue) {
+    return env("TRACE_REPORT_URL", defValue);
+  }
+
+  public static String testName(String defValue) {
+    return env("TEST_NAME", defValue);
+  }
+
+  public static double scalingRatio(double d) { // TODO Auto-generated method stub
+	  return Double.valueOf(env("SCALE_RATIO", String.valueOf(d)));
+  }
+}
---
+++ src/main/java/io/scalecube/trace/service/reporter/latency/PerfServiceLatencyListener.java
@@ -0,0 +1,47 @@
+package io.scalecube.trace.service.reporter.latency;
+
+import io.scalecube.trace.EnviromentVariables;
+import io.scalecube.trace.TraceReporter;
+import io.scalecube.trace.service.reporter.PerfromanceReporter;
+import org.HdrHistogram.Histogram;
+
+public class PerfServiceLatencyListener implements LatencyListener {
+
+
+  private final TraceReporter reporter;
+  private final String testName = EnviromentVariables.testName("TEST_NAME");
+  private final String commitId = EnviromentVariables.sha("1");
+  private final double scalingRatio = EnviromentVariables.scalingRatio(1000.0) ; // microseconds
+  private double[] percentiles;
+  
+  public PerfServiceLatencyListener(double... percentiles) {
+    this.reporter = new TraceReporter();
+    this.percentiles = percentiles;
+  }
+
+  @Override
+  public void onReport(Histogram histogram) {
+    for (double d : percentiles) {
+      reporter.addY(
+          "[p" + d + "] " + this.testName,
+          "latency",
+          histogram.getValueAtPercentile(d) / scalingRatio);
+    }
+  }
+
+  @Override
+  public void close() {
+    try {
+      if (commitId != null && !commitId.trim().isEmpty()) {
+        PerfromanceReporter.publish(reporter);
+      }
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  @Override
+  public void onTerminate(Histogram accumulatedHistogram) {
+    // nothing to do here
+  }
+}
